d05fcc0b4c58211eff49f253a56f31cfd6a0dba5
Branislav.Macosinec <Branislav.Macosinec@immediate.co.uk>
2019-02-18 16:34:42 +0000

    Modified Index method in HomeController to pass HomeViewModel to View

--- BethanyPieShop/Controllers/HomeController.cs
+++ BethanyPieShop/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -18,8 +20,12 @@ namespace BethanyPieShop.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.Title = "Pie Overview";
             var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
             return View(pies);
         }
     }
