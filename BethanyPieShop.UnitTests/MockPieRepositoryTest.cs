0f3148b5b0e1fbd6a411a7551f1cfc2039b4f03e
Branislav.Macosinec <Branislav.Macosinec@immediate.co.uk>
2019-02-19 13:01:23 +0000

    MockPieRepositoryTest with xUnit tests and use Fluent Assertion

---
+++ BethanyPieShop.UnitTests/MockPieRepositoryTest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BethanyPieShop.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace BethanyPieShop.UnitTests
+{
+    public class MockPieRepositoryShould
+    {
+        [Fact(DisplayName = nameof(MockPieRepositoryShould) + " _ " + nameof(Have_SomePies))]
+        public void Have_SomePies()
+        {
+            MockPieRepository pieRepository = new MockPieRepository();
+            var pies = pieRepository.GetAllPies().Count();
+            Assert.Equal(4,pies);
+        }
+
+        [Fact(DisplayName = nameof(MockPieRepositoryShould) + " _ " + nameof(NotToBeEmpty))]
+        public void NotToBeEmpty()
+        {
+            MockPieRepository pieRepository = new MockPieRepository();
+            var pies = pieRepository.GetAllPies().Count();
+            pies.Should().BePositive();
+        }
+
+        [Fact(DisplayName = nameof(MockPieRepositoryShould) + " _ " + nameof(BeTypeOf_List))]
+        public void BeTypeOf_List()
+        {
+            MockPieRepository pieRepository = new MockPieRepository();
+            var pies = pieRepository.GetAllPies();
+            pies.Should().BeOfType(typeof(List<Pie>));
+        }
+
+        [Fact(DisplayName = nameof(MockPieRepositoryShould) + " _ " + nameof(NotToBe_Empty))]
+        public void NotToBe_Empty()
+        {
+            MockPieRepository pieRepository = new MockPieRepository();
+            var pies = pieRepository.GetAllPies();
+            pies.Should().NotBeNull();
+        }
+    }
+}
+ 
