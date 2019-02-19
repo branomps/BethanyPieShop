1e125d570299bf8e2a65bd09f84a63434e44f5c8
Branislav.Macosinec <Branislav.Macosinec@immediate.co.uk>
2019-02-19 15:40:30 +0000

    Change model , add migration and update model.

--- BethanyPieShop/Migrations/20190219153800_PieModelChange.Designer.cs
+++ BethanyPieShop/Migrations/20190219153800_PieModelChange.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BethanyPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190219153800_PieModelChange")]
+    partial class PieModelChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
