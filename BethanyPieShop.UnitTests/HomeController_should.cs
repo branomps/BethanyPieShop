3a78f5cd77e0fe33eabb2bc3ae13ed0a071e4a38
Branislav.Macosinec <Branislav.Macosinec@immediate.co.uk>
2019-02-20 13:17:20 +0000

    Add xUnit test for HomeController.

---
+++ BethanyPieShop.UnitTests/HomeController_should.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BethanyPieShop.Controllers;
+using BethanyPieShop.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace BethanyPieShop.UnitTests
+{
+    public class HomeController_should
+    {
+        [Fact]
+        public void Return_ViewModel_from_Index()
+        {
+            var repository = Mock.Of<IPieRepository>();
+            Mock.Get(repository)
+                .Setup(r => r.GetAllPies())
+                .Returns(
+                    new[]
+                    {
+                        new Pie
+                        {
+                            Id = 1,
+                            Name = "Test Pie"
+                        },
+                    });
+
+            var sut = new HomeController(repository);
+
+            sut.Index().Should().BeEquivalentTo(
+                new {
+                    ViewName = (string)null,
+                    Model = new {
+                        Title = "Welcome to Bethany's Pie Shop",
+                        Pies = new[]
+                        {
+                            new
+                            {
+                                Id = 1,
+                                Name = "Test Pie "
+                            }
+                        }
+                    }
+                });
+        }
+    }
+}
+
